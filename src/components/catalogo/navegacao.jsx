b0db9262b1f238dfe0e88eef1f5fe72f4a7a6b54
Lucas Justino <lucas.justino.dev@gmail.com>
2024-08-19 15:25:58 -0300

    Criação da Notificação de Quantidade de Itens do Carrinho

--- src/components/catalogo/navegacao.jsx
+++ src/components/catalogo/navegacao.jsx
@@ -1,7 +1,9 @@
+import { useContext } from "react";
 import Logo from "./logo";
 import { FiSearch } from "react-icons/fi";
 import { FiShoppingBag } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import { CarrinhoContext } from "../context/carrinhoContext";
 
 const Navegacao = () => {
   const navigate = useNavigate();
@@ -10,6 +12,8 @@ const Navegacao = () => {
     navigate('/carrinho')
   };
 
+  const {carrinho} = useContext(CarrinhoContext);
+
   return (
     <div className="flex justify-between items-center bg-white h-20 pt-[19px] px-36">
       <Logo />
@@ -17,9 +21,14 @@ const Navegacao = () => {
         Pesquise aqui
         <FiSearch size={20} />
       </div>
-      <button onClick={handleCarrinhoClick}>
-        <FiShoppingBag color="gray" size={20} />
-      </button>
+      <button onClick={handleCarrinhoClick} className="relative">
+      <FiShoppingBag color="gray" size={20} />
+      {carrinho.length > 0 && (
+        <span className="absolute bottom-[-8px] right-[-8px] bg-[#F25D27] text-white text-[10px] rounded-full h-4 w-4 flex items-center justify-center">
+          {carrinho.length}
+        </span>
+      )}
+    </button>
     </div>
   );
 };
